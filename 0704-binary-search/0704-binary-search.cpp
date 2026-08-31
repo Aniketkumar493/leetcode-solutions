6946772d64a0e4393a4515c48d1975451fcc4aaa
Aniket kumar <151552344+Aniketkumar493@users.noreply.github.com>
2026-09-01 00:00:41 +0530

    Time: 0 ms (100%), Space: 31.4 MB (46.2%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return binary(nums, 0, nums.size() - 1, target);
+    }
+
+private:
+    int binary(vector<int>& nums, int left, int right, int target) {
+        if (left > right)
+            return -1;
+
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+
+        if (target < nums[mid])
+            return binary(nums, left, mid - 1, target);
+
+        return binary(nums, mid + 1, right, target);
+    }
+};
